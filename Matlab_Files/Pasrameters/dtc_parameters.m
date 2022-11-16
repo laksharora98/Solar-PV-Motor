84dfa8c4302e41e5c050427a9ebc3af56751e590
laksharora98 <91553008+laksharora98@users.noreply.github.com>
2022-11-16 20:29:26 +0530

    Add files via upload

---
+++ Matlab_Files/Pasrameters/dtc_parameters.m
@@ -0,0 +1,12 @@
+Lr = 0.0430;
+Ls =  0.0430;
+Lm = 0.0416;
+Ls1 = 0.0027544;
+Rs = 0.251;
+%t = get_param('DTCIMD_30hp','SimulationTime');
+%while (t<1.1)
+ %   pause(0.1)
+  %  t = get_param('DTCIMD_30hp','SimulationTime');
+%end 
+%Rs = 0.251*1.3
+%t
